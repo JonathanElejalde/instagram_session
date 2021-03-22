bb73c16bcbf1569560020810ff3d982995815947
JonathanElejalde <jonathanelejalde@hotmail.com>
2021-03-22 15:42:53 -0500

    Now we commit the changes

--- database.py
+++ database.py
@@ -19,31 +19,33 @@ conn = sqlite3.connect('instagram.db')
 cur = conn.cursor()
 cur.execute("PRAGMA foreign_keys = ON;")
 cur.executescript(
-    """
+"""
 CREATE TABLE IF NOT EXISTS User(
 	username TEXT NOT NULL UNIQUE PRIMARY KEY
- );
+);
 
- CREATE TABLE IF NOT EXISTS Following(
-     username TEXT NOT NULL,
-     followed_by TEXT NOT NULL,
-     FOREIGN KEY (followed_by) REFERENCES User (username)
- );
+CREATE TABLE IF NOT EXISTS Following(
+    username TEXT NOT NULL,
+    followed_by TEXT NOT NULL,
+    FOREIGN KEY (followed_by) REFERENCES User (username)
+);
 
- CREATE TABLE IF NOT EXISTS Visited(
-     username TEXT NOT NULL,
-     photos_liked INTEGER,
-     followed_by TEXT NOT NULL,
-     unfollow INTEGER DEFAULT NULL,
-     FOREIGN KEY (followed_by) REFERENCES User (username)
- );
+CREATE TABLE IF NOT EXISTS Visited(
+    username TEXT NOT NULL,
+    photos_liked INTEGER,
+    followed_by TEXT NOT NULL,
+    unfollow INTEGER DEFAULT NULL,
+    FOREIGN KEY (followed_by) REFERENCES User (username)
+);
 
- CREATE TABLE IF NOT EXISTS Photos(
-     link TEXT NOT NULL,
-     liked_by TEXT NOT NULL,
-     FOREIGN KEY (liked_by) REFERENCES User (username)
- );
- """)
+CREATE TABLE IF NOT EXISTS Photos(
+    link TEXT NOT NULL,
+    liked_by TEXT NOT NULL,
+    FOREIGN KEY (liked_by) REFERENCES User (username)
+);
+""")
+
+conn.commit()
 
 
 if __name__ == "__main__":
