22f0e86a0fcf676d05146d4097f6980c0d8045c6
JonathanElejalde <jonathanelejalde@hotmail.com>
2021-04-29 17:32:51 -0500

    Added annotations and more helper functions for cleaner code

--- instagram.py
+++ instagram.py
@@ -7,69 +7,80 @@ import instaloader
 
 
 class Instagram:
+    """Class for handling instagram functionalities"""
+
     instagram_link = "https://www.instagram.com/"
 
-    def __init__(self, username, password, driver):
+    def __init__(self, username: str, password: str, driver: webdriver) -> None:
         self.username = username
         self.driver = driver
         self.password = password
 
         # Login to instaloader
-        self.L = instaloader.Instaloader()
-        self.L.login(self.username, self.password)
+        self.profile = self._login_instaloader()
+
+        # Login to instagram
+        self._login_instagram()
 
         # delete password
         del password
         del self.password
 
-    def create_link(self, username):
+    def _login_instaloader(self):
+        profile = instaloader.Instaloader()
+        profile.login(self.username, self.password)
+
+        return profile
+
+    def _get_instaloader_metadata(self, username):
+        metadata = instaloader.Profile.from_username(self.profile.context, username)
+
+        return metadata
+
+    def _check_current_url(self, link: str) -> bool:
+        """Checks if current link is the same as the required
+        link"""
+
+        return self.driver.current_url == link
+
+    def create_link(self, username: str) -> str:
         """
-        It takes the username of an instagram account and creates a
-        link to the homepage of that username
+        Creates a instagram homepage link with `username`
         """
         if "@" in username:
-            username = username.replace('@', '')
-        link = self.instagram_link + username + '/'
+            username = username.replace("@", "")
+        link = self.instagram_link + username + "/"
 
         return link
 
-    def login(self, password):
-        """The login function allows us to log in an instagram account."""
+    def _get_username(self, link: str) -> str:
+        username = link.split("/")[-2]
+        return username
 
-        # Load the login page
+    def _login_instagram(self) -> None:
+        """Login an instagram account with selenium"""
+
+        # Login page
         self.driver.get(self.instagram_link + "accounts/login/")
 
-        # Fill the format with username and password
+        # Fill login format
         username = self.driver.find_element_by_name("username")
         username.clear()
         username.send_keys(self.username)
         passw = self.driver.find_element_by_name("password")
         passw.clear()
-        passw.send_keys(password)
+        passw.send_keys(self.password)
         passw.send_keys(Keys.RETURN)
 
-    def get_followers(self, username, number_of_accounts=None):
+    def get_followers(self, username: str, number_of_accounts: set = None) -> set:
         """
         Gets all the accounts that are following `username` and
-        returns the `number_of_accounts` specified
-
-        Parameters
-        ----------
-        username : str
-        number_of_accounts : int
-            Amount of followers to return. If None, 
-            it returns all
-
-        Returns
-        -------
-            A set with the usernames to follow 
+        returns the specified `number_of_accounts`
         """
-        # Get profile metadata of the user
-        self.metadata = instaloader.Profile.from_username(self.L.context, username)
+        metadata = self._get_instaloader_metadata(username)
 
-        # get accounts
         accounts = set()
-        for i, users in enumerate(self.metadata.get_followers()):
+        for i, users in enumerate(metadata.get_followers()):
             accounts.add(users.username)
             if number_of_accounts == None:
                 continue
@@ -79,51 +90,36 @@ class Instagram:
 
         return accounts
 
-    def get_followees(self, username):
+    def get_followees(self, username: str) -> set:
         """
-        Gets the accounts that the username is following
-
-        Returns
-        -------
-            A set with the usernames that the user is following 
+        Gets the accounts that `username` is following
         """
-        # Get profile metadata of the user
-        self.metadata = instaloader.Profile.from_username(self.L.context, username)
+        metadata = self._get_instaloader_metadata(username)
 
         # get accounts
         accounts = set()
-        for users in self.metadata.get_followees():
+        for users in metadata.get_followees():
             accounts.add(users.username)
 
         return accounts
 
-    def get_photos(self, link):
+    def get_photos(self, link: str) -> list:
         """
-        Returns a random amount of links with the photos of the `link` that we are searching
-
-        Parameters
-        ----------
-        link : str
-            The homepage of the instagram account from where we want to get the photos
-
-        Returns
-        -------
-            List with the profile photos to like or None when the profile doesn't have photos or it is private
+        Returns a random amount of instagram photo from the current link or None
+        if there are not photos or the account is private
         """
-        # self.driver.get(link)
 
-        time.sleep(2)
+        if not self._check_current_url(link):
+            self.driver.get(link)
+            time.sleep(2)
 
         # Scroll down the page to obtain more photos
         for i in range(1, 3):
-            self.driver.execute_script(
-                "window.scrollTo(0, document.body.scrollHeight)")
+            self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
             time.sleep(1)
 
-        # Wait after scroll down in the profile
-        time.sleep(random.randint(1, 4))
+        time.sleep(random.randint(1, 3))
 
-        # We add a try and except in case that the profile is private in that case we are gonna return None
         try:
 
             photos = []
@@ -132,19 +128,20 @@ class Instagram:
             for photo in self.driver.find_elements_by_tag_name("a"):
                 p = photo.get_attribute("href")
 
-                # If the link is a photo we add it to photos. A maximum of 10
+                # Checks photos. Maximum 10
                 if p.startswith("https://www.instagram.com/p/") and count <= 10:
                     photos.append(p)
                     count += 1
 
-            # From the photos that we collect, we choose randomly which one to like
+            # Amount of photos we will return
             likes = random.randint(2, 4)
 
+            # Private or empty account
             if len(photos) == 0:
+                username = self._get_username(link)
+                print(f"The account {username} is private or empty")
                 return None
 
-            # If the profile has less photos than the number of likes, likes become the len of the list
-            # Shuffle the list to return the photos in a random order
             if likes > len(photos):
                 likes = len(photos)
                 random.shuffle(photos)
@@ -159,160 +156,139 @@ class Instagram:
 
         except:
             # If an error we assume is private or does not exists
-            username = link.split("/")[-2]
-            print(
-                f"The account {username} is private, does not have photos or does not exists")
+            username = self._get_username(link)
+            print(f"The account {username} is private, empty or does not exist")
             return None
 
-    def like_photo(self, link):
+    def like_photo(self, link: str) -> str:
         """
-        Gives a like to one photo.
-
-            Parameters
-            ----------
-            link : str
-                The link of the photo
+        Likes an instagram photo
         """
 
-        self.driver.get(link)
-        time.sleep(1)
+        if not self._check_current_url(link):
+            self.driver.get(link)
+            time.sleep(2)
 
         try:
             self.driver.find_element_by_css_selector(
-                ".fr66n > button:nth-child(1)").click()
+                ".fr66n > button:nth-child(1)"
+            ).click()
         except Exception as e:
             print("error in like function")
             print(e)
 
-    def follow(self, link):
+    def follow(self, link: str) -> None:
         """
         Follows the given user
-
-            Parameters
-            ----------
-            link : str
-                Homepage of the user 
         """
 
-        time.sleep(1)
-        #self.driver.get(link)
+        if not self._check_current_url(link):
+            self.driver.get(link)
+            time.sleep(2)
+
         # We move to the top of the page
         Keys.HOME
         try:
             Keys.HOME
             time.sleep(2)
-            follow_button = self.driver.find_element_by_xpath(
-                "/html/body/div[1]/section/main/div/header/section/div[1]/div[1]/div/div/div/span/span[1]/button")
+            element = "/html/body/div[1]/section/main/div/header/section/div[1]/div[1]/div/div/div/span/span[1]/button"
+            follow_button = self.driver.find_element_by_xpath(element)
             # follow_button = self.driver.find_elements_by_css_selector("._6VtSN")
             follow_button.click()
             time.sleep(1)
         except Exception as e:
             print(e)
         else:
-            user = link.split('/')[-2]
+            user = link.split("/")[-2]
             print("You followed {}".format(user))
 
-    def unfollow(self, link):
+    def unfollow(self, link: str) -> None:
         """
-        Unfollows the given user
-
-            Parameters
-            ----------
-            username : str
-                The username of the instagram account
-
-            Returns
-            -------
-            A string specifying which account was unfollowed 
+        Unfollows an account
         """
+        if not self._check_current_url(link):
+            self.driver.get(link)
+            time.sleep(2)
 
-        self.driver.get(link)
-        time.sleep(2)
-
-        follow_button = self.driver.find_element_by_css_selector(".glyphsSpriteFriend_Follow")
-        #follow_button = self.driver.find_element_by_class_name('_5f5mN    -fzfL     _6VtSN     yZn4P   ')
-        #follow_button = self.driver.find_element_by_xpath("/html/body/div[1]/section/main/div/header/section/div[1]/div[1]/div/div[2]/div/span/span[1]/button")
+        element = ".glyphsSpriteFriend_Follow"
+        follow_button = self.driver.find_element_by_css_selector(element)
         follow_button.click()
+
         time.sleep(1)
+
         confirmation_button = self.driver.find_element_by_xpath(
-            '/html/body/div[5]/div/div/div/div[3]/button[1]'
+            "/html/body/div[5]/div/div/div/div[3]/button[1]"
         )
         confirmation_button.click()
 
-        return print(f"You unfollowed: {link.split('/')[-2]}")
+        print(f"You unfollowed: {link.split('/')[-2]}")
 
-    def comment(self, comment_list):
+    def comment(self, comment_list: list, link: str) -> None:
         """
-        Choose a comment from a given list to comment a photo that you liked.
+        Choose a comment from the given list an comment on `link`
+        """
 
-        Parameters
-        ----------
-        comment_list : list
-            List with the possible comments 
-        """
-        comment = True
+        if not self._check_current_url(link):
+            self.driver.get(link)
+            time.sleep(2)
+
+        disabled = False
+        # Check disabled comments
         try:
-            enter_key = u'\ue007'
-            # get the comment field and click
-            comment_field = self.driver.find_element_by_class_name(
-                "Ypffh").click()
+            enter_key = "\ue007"
+
+            # gets the comment field and clicks
+            comment_field = self.driver.find_element_by_class_name("Ypffh").click()
+
             # self.driver.execute_script(
             #    'window.scrollTo(0, document.body.scrollHeight)')
         except:
             print("The comments are disabled")
-            comment = False
-        
-        if comment:
-            # If we can click on the text box, check if we can add the comment
+            disabled = True
+
+        if not disabled:
             try:
                 seconds = random.randint(1, 3)
                 time.sleep(seconds)
 
-                # From the list of comments get one
+                # Get one comment
                 comment_text = random.choice(comment_list)
 
-                # We have to search for the field again to add the comment, then enter
+                # We have to search for the field again
                 comment_field = self.driver.find_element_by_css_selector(".Ypffh")
                 comment_field.send_keys(comment_text + enter_key)
 
                 time.sleep(random.randint(1, 3))
             except Exception as e:
-                print('Error adding the comment')
+                print("Error adding the comment")
                 print(e)
 
-                
-
-    def already_follow(self, link):
+    def already_follow(self, link: str) -> bool:
         """
-        Checks if we can or cannot follow the account
-
-        Parameters
-        ----------
-        username : str
-            The homepage link of the account
-
-        Returns
-        -------
-        follow : boolean 
+        Checks if the current account follows the target account.
+        Returns True if need to follow
         """
 
-        self.driver.get(link)
-        time.sleep(2)
-        follow = False
+        if not self._check_current_url(link):
+            self.driver.get(link)
+            time.sleep(2)
 
-        # We identify the follow button and then we decide what to do
+        need_follow = False
+
         try:
-            follow_button = self.driver.find_element_by_css_selector(".BY3EC")
+            element = ".BY3EC"
+            follow_button = self.driver.find_element_by_css_selector(element)
+
             # Check for "Seguir" or "Follow". Depends on the user language
             if follow_button.text == "Seguir" or follow_button.text == "Follow":
-                follow = True
-                return follow
+                need_follow = True
+                return need_follow
             else:
-                return follow
+                return need_follow
 
         except:
             # If there is an error, return False to continue with another user
-            return follow
+            return need_follow
 
     def close(self):
         """close the driver connection"""
